2cf1bcde0eb07f70f7b498fad55a955c636180bd
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-08-09 18:00:52 -0300

    fix: Adiciona tratamento de responsividade para o component de hero section
    Foram adicionados classes tailwind adicionais para tratar a responsividade
    
    Wrap nos elementos da hero

--- kortex-app/src/app/sections/hero-section/grid.tsx
+++ kortex-app/src/app/sections/hero-section/grid.tsx
@@ -1,48 +1,48 @@
 import { Compare } from "@/components/ui/compare";
 import {
-  TextRevealCardTitle,
-  TextRevealCardDescription,
-  TextRevealCard,
+	TextRevealCardTitle,
+	TextRevealCardDescription,
+	TextRevealCard,
 } from "@/components/ui/text-reveal-card";
 import Design from "@/assets/design.svg";
 import Image from "next/image";
 
 export function GridComponent() {
-  return (
-    <div className="w-[712px] flex flex-col gap-5">
-      <div className="flex gap-5 w-full h-[440px]">
-        <div className="bg-grey-color border border-white/[0.08] rounded-3xl text-p-color w-1/2 p-10 h-full flex align-center gap-2 flex-col justify-center">
-          <Image src={Design} alt="Design sob medida image" />
-          <TextRevealCardTitle>Design sob medida</TextRevealCardTitle>
+	return (
+		<div className="w-[712px] flex flex-col gap-5">
+			<div className="flex gap-5 flex-col md:flex-row w-full ">
+				<div className="bg-grey-color w-full border border-white/[0.08] rounded-3xl text-p-color md:w-1/2 p-10 flex align-center gap-2 flex-col justify-center min-h-fit">
+					<Image src={Design} alt="Design sob medida image" />
+					<TextRevealCardTitle>Design sob medida</TextRevealCardTitle>
 
-          <TextRevealCardDescription>
-            Cada projeto é uma obra-prima, desenvolvida para refletir a essência
-            da sua marca com estética refinada e funcionalidade excepcional.
-          </TextRevealCardDescription>
-        </div>
-        <div className="bg-purple-gradient border flex gap-8 items-center flex-col border-white/[0.08] rounded-3xl text-p-color w-1/2 p-10 h-full">
-          <TextRevealCardTitle>Código Robusto e Escalável</TextRevealCardTitle>
-          <Compare
-            firstImage="https://assets.aceternity.com/code-problem.png"
-            secondImage="https://assets.aceternity.com/code-solution.png"
-            firstImageClassName="object-cover object-left-top"
-            secondImageClassname="object-cover object-left-top"
-            className="h-[150px] w-[100px] md:h-[300px] md:w-[300px] align-bottom"
-            slideMode="hover"
-          />
-        </div>
-      </div>
-      <div>
-        <TextRevealCard text={"Seu Projeto"} revealText={"Nossa estrutura"}>
-          <TextRevealCardTitle>
-            Suporte Técnico e Manutenção
-          </TextRevealCardTitle>
-          <TextRevealCardDescription>
-            Oferecemos suporte técnico contínuo e serviços de manutenção para
-            garantir que sua presença online esteja sempre atualizada e segura.
-          </TextRevealCardDescription>
-        </TextRevealCard>
-      </div>
-    </div>
-  );
+					<TextRevealCardDescription>
+						Cada projeto é uma obra-prima, desenvolvida para refletir a essência
+						da sua marca com estética refinada e funcionalidade excepcional.
+					</TextRevealCardDescription>
+				</div>
+				<div className="w-full bg-purple-gradient border flex gap-8 items-center flex-col border-white/[0.08] rounded-3xl text-p-color md:w-1/2 p-10 min-h-80">
+					<TextRevealCardTitle>Código Robusto e Escalável</TextRevealCardTitle>
+					<Compare
+						firstImage="https://assets.aceternity.com/code-problem.png"
+						secondImage="https://assets.aceternity.com/code-solution.png"
+						firstImageClassName="object-cover object-left-top"
+						secondImageClassname="object-cover object-left-top"
+						className="h-[250px] w-[250px] lg:h-[280px] lg:w-[280px] align-bottom"
+						slideMode="hover"
+					/>
+				</div>
+			</div>
+			<div>
+				<TextRevealCard text={"Seu Projeto"} revealText={"Nossa estrutura"}>
+					<TextRevealCardTitle>
+						Suporte Técnico e Manutenção
+					</TextRevealCardTitle>
+					<TextRevealCardDescription>
+						Oferecemos suporte técnico contínuo e serviços de manutenção para
+						garantir que sua presença online esteja sempre atualizada e segura.
+					</TextRevealCardDescription>
+				</TextRevealCard>
+			</div>
+		</div>
+	);
 }
--- kortex-app/src/app/sections/hero-section/index.tsx
+++ kortex-app/src/app/sections/hero-section/index.tsx
@@ -3,13 +3,13 @@ import { GridComponent } from "./grid";
 import { InfoComponent } from "./info-logo";
 
 export function HeroSection() {
-  return (
-    <section className="flex justify-center w-full items-center h-[100vh]">
-      <BackgroundBeams className="absolute z-0" />
-      <div className="flex items-center w-full justify-around z-10 p-28 gap-y-24">
-        <InfoComponent />
-        <GridComponent />
-      </div>
-    </section>
-  );
+	return (
+		<section className="flex justify-center w-full items-center h-fit">
+			<BackgroundBeams className="absolute z-0" />
+			<div className="flex flex-wrap items-center w-full justify-around z-10 p-10 md:p-28 gap-y-24">
+				<InfoComponent />
+				<GridComponent />
+			</div>
+		</section>
+	);
 }
