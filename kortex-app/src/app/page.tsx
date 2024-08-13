c62aa02fec7dfeec6cb5d0a49b48857cdfdee757
Mateus Magno <mateusmagnoferreirasoares@gmail.com>
2024-08-12 23:21:45 -0300

    feat(Contact Section): Contact Section FrontEnd completo
    Agora o front end da contact-section esta completo, bastando ajustar as funcionalidades tais como linkagem de botões e responsividade

--- kortex-app/src/app/page.tsx
+++ kortex-app/src/app/page.tsx
@@ -1,13 +1,17 @@
 import { HeroSection } from "./sections/hero-section";
 import PortfolioSection from "./sections/portfolio-section";
 import RatingsSection from "./sections/ratings-section";
+import ContactSection from "./sections/contact-section";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
-	return (
-		<div className="bg-custom-gradient min-h-screen">
-			<HeroSection />
-			<PortfolioSection />
-			<RatingsSection />
-		</div>
-	);
+  return (
+    <div className="bg-custom-gradient min-h-screen flex flex-col gap-20">
+      <HeroSection />
+      <PortfolioSection />
+      <RatingsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
 }
