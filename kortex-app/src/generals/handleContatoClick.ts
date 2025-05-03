b1c40f58c2511de6c3fbda6331cc51a5c0d7ec2b
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2025-05-03 13:00:00 -0300

    refactor: takes out last kortex site

--- kortex-app/src/generals/handleContatoClick.ts
+++
@@ -1,9 +0,0 @@
-export function handleContatoClick() {
-  const telefones = [5561999440733, 5561999818682];
-  let num = Math.round(Math.random());
-  window.open(
-    // 5561999440733
-    `https://wa.me/${telefones[num]}?text=Tenho%20interesse%20em%20fazer%20um%20orçamento`,
-    "_blank"
-  );
-}
--- kortex-app/src/generals/handleScroll.ts
+++
@@ -1,8 +0,0 @@
-export function handleScroll(id: string) {
-  let topValue = document.querySelector(id)?.getBoundingClientRect().top;
-  const newTopValue = topValue !== undefined ? topValue - 100 : undefined;
-  window.scrollTo({
-    top: newTopValue,
-    behavior: "smooth",
-  });
-}
