9d5412c72a73f553841485f498f284304d4f41b5
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2025-05-11 22:27:14 -0300

    fix: fix some responsivity

--- front-end/src/app/footer.tsx
+++ front-end/src/app/footer.tsx
@@ -4,39 +4,44 @@ import { FiGithub } from "react-icons/fi";
 import Image from "next/image";
 
 function Footer() {
-	return (
-		<footer className="w-full h-20 flex items-center justify-center text-p-color mb-20">
-			<div className="flex w-full max-w-[1240px] md:flex-row flex-col-reverse items-center gap-4 justify-around">
-				<p>&copy; 2025 KorteX.</p>
-				<div className="flex justify-between w-60">
-					<a href="https://www.instagram.com/kortex_tecnologia/" target="blank">
-						<FaInstagram className="text-4xl" />
-					</a>
-					<a href="https://github.com/KorteX-Devs" target="blank">
-						<FiGithub className="text-4xl" />
-					</a>
-					<a href="#">
-						<FaWhatsapp className="text-4xl" />
-					</a>
-					<a href="#">
-						<FaLinkedinIn className="text-4xl" />
-					</a>
-				</div>
-				<button>
-					<div className="flex items-center gap-6">
-						<Image
-							src="assets/kortex-logo.svg"
-							alt="Kortex Logo"
-							width={50}
-							height={50}
-						/>
-						<p className="font-medium text-2xl text-zinc-50">Kortex</p>
-					</div>
-				</button>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="w-full h-20 flex items-center justify-center text-p-color mb-20">
+      <div className="flex w-full max-w-[1240px] md:flex-row flex-col-reverse items-center gap-4 justify-around">
+        <p>&copy; 2025 KorteX.</p>
+        <div className="flex justify-between w-60">
+          <a
+            href="https://www.instagram.com/kortex_tecnologia/"
+            target="_blank"
+          >
+            <FaInstagram className="text-4xl" />
+          </a>
+          <a href="https://github.com/KorteX-Devs" target="_blank">
+            <FiGithub className="text-4xl" />
+          </a>
+          <a href="https://wa.link/o16z7c" target="_blank">
+            <FaWhatsapp className="text-4xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/kortex-tecnologia"
+            target="_blank"
+          >
+            <FaLinkedinIn className="text-4xl" />
+          </a>
+        </div>
+        <button>
+          <div className="flex items-center gap-6">
+            <Image
+              src="assets/kortex-logo.svg"
+              alt="Kortex Logo"
+              width={50}
+              height={50}
+            />
+            <p className="font-medium text-2xl text-zinc-50">Kortex</p>
+          </div>
+        </button>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
-
--- front-end/src/app/hero-section.tsx
+++ front-end/src/app/hero-section.tsx
@@ -1,30 +1,35 @@
+"use client";
+
 import React from "react";
+import { scrollIntoView } from "./header";
 
 function HeroSection() {
-	return (
-		<section className="w-full items-center justify-center gap-15 max-h-screen flex flex-col">
-			<h1 className="text-center">
-				Crescer com consistência. <br />
-				<span className="bg-linear-to-r from-[#473BF0] via-[#00E2FD] to-[#871AFE] bg-clip-text text-transparent">
-					{" "}
-					Escalar com confiança
-				</span>
-			</h1>
-			<p className="text-2xl text-center max-w-[700px]">
-				Sistemas web bem projetados para atender às demandas atuais do seu
-				negócio, com uma arquitetura sólida e flexível, preparada para
-				acompanhar seu crescimento
-			</p>
-			<div className="flex flex-col gap-4 items-center">
-				{" "}
-				<button className="text-md bg-[#4343f15e] px-12 py-5 rounded-lg ">
-					Conheça os serviços
-				</button>
-				<p>Atuando desde 2022</p>
-			</div>
-		</section>
-	);
+  return (
+    <section className="w-full items-center justify-center gap-15 max-h-screen flex flex-col z-50">
+      <h1 className="text-center">
+        Crescer com consistência. <br />
+        <span className="bg-linear-to-r from-[#473BF0] via-[#00E2FD] to-[#871AFE] bg-clip-text text-transparent">
+          {" "}
+          Escalar com confiança
+        </span>
+      </h1>
+      <p className="text-2xl text-center max-w-[700px]">
+        Sistemas web bem projetados para atender às demandas atuais do seu
+        negócio, com uma arquitetura sólida e flexível, preparada para
+        acompanhar seu crescimento
+      </p>
+      <div className="flex flex-col gap-4 items-center">
+        {" "}
+        <button
+          onClick={() => scrollIntoView("servicos")}
+          className="text-md bg-[#4343f15e] px-12 py-5 rounded-lg cursor-pointer hover:bg-[#4343f191] duration-300"
+        >
+          Conheça os serviços
+        </button>
+        <p>Atuando desde 2022</p>
+      </div>
+    </section>
+  );
 }
 
 export default HeroSection;
-
